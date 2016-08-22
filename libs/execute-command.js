d8fa2533de01b44674c753bcc89e161403d03e02
Masayoshi Nomura <nmrmsys@gmail.com>
2016-08-22 12:55:44 +0900

    Fix require path for global install

--- libs/execute-command.js
+++ libs/execute-command.js
@@ -6,20 +6,20 @@ module.exports = function (command, options) {
   sphero = require('./sphero-instance')();
   if (sphero) {
     sphero.connect(function () {
-      require(appRootPath + '/libs/commands/' + command)(sphero, options);
+      require('./commands/' + command)(sphero, options);
     });
   }
 };
 
 module.exports.alreadyConnected = function(sphero, command) {
   if (sphero) {
-    require(appRootPath + '/libs/commands/' + command)(sphero);
+    require('./commands/' + command)(sphero);
   }
 };
 
 module.exports.alreadyConnectedSingleValue = function(sphero, command, options) {
   if (sphero) {
-    require(appRootPath + '/libs/commands/' + command)(sphero, options);
+    require('./commands/' + command)(sphero, options);
   }
 };
 
@@ -27,7 +27,7 @@ module.exports.alreadyConnectedMultipleValues = function(sphero, command, option
 
   if (sphero) {
     var parameters = _.union([sphero], options);
-    require(appRootPath + '/libs/commands/' + command).apply(this, parameters);
+    require('./commands/' + command).apply(this, parameters);
   }
 
 };
